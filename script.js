14a97e22c2a4bf7ea46cef4e840136ee92e214a6
mac <mac@Noutbuk-mac.local>
2026-05-23 20:34:51 +0300

    hw2

--- script.js
+++ script.js
@@ -1,52 +1,22 @@
-// ---task 1---
-let km = prompt("enter km: ");
-function kmToMile(km) {
-    console.log("miles: ", km * 0.621371);
-}
+//--- task 1 ---
+let num = prompt("Enter a number");
+console.log(num**2);
 
-kmToMile(km)
+//---- task 2 ---
+//a)
+let num1 = prompt("Enter a number1");
+let num2 = prompt("Enter a number2");
+let convert1 = +num1;
+let convert2 = +num2;
+console.log((convert1+convert2)/2);
 
-// ---task 2---
-let num1 = prompt("enter num1: ");
-let num2 = prompt("enter num2: ");
-function calc(num1, num2) {
-    let convert1 = +num1;
-    let convert2 = +num2;
-    if (!isNaN(convert1) && !isNaN(convert2)) {
-        console.log(num1, " + ", num2, " = ", convert1 + convert2);
-        console.log(num1, " - ", num2, " = ", convert1 - convert2);
-        console.log(num1, " * ", num2, " = ", convert1 * convert2);
-        console.log(num1, " / ", num2, " = ", convert1 / convert2);
-    }
-    else {
-        return null
-    }
-}
+//b)
+let a = prompt("Enter a number1");
+let b = prompt("Enter a number2");
+//a * x + b = 0
+//x = -b/a
+console.log("x = ", -b/a);
 
-calc(num1, num2);
-
-// ---task 3---
-let age = prompt("enter age: ");
-
-function whoAreYou(age) {
-    let youAge = +age;
-    if (!isNaN(youAge)) {
-        if (youAge >= 0 && youAge < 12) {
-            console.log("ти дитина");
-        }
-        else if (youAge >= 12 && youAge < 18) {
-            console.log("ти підліток");
-        }
-        else if (youAge >= 18 && youAge < 60) {
-            console.log("ти дорослий");
-        }
-        else if (youAge >= 60) {
-            console.log("ти пенсіонер");
-        }
-    }
-    else {
-        return null
-    }
-}
-
-whoAreYou(age);
+//c)
+let salary = prompt("ваши продажи: ");
+console.log(250 + (salary * 0.10));
