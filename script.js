64eeaf55c7adcd57034aa61e19b721fe62d06ee9
Ivan Fede <68062115+vania0836@users.noreply.github.com>
2026-06-10 23:25:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 // Завдання 1
-var b1 = document.getElementById('b1');
-var b2 = document.getElementById('b2');
-var b3 = document.getElementById('b3');
-var bottom = document.getElementById('bottom');
-
-b1.addEventListener('mouseover', function () {
-    bottom.style.background = '#4cdd1f';
-});
-b2.addEventListener('mouseover', function () {
-    bottom.style.background = '#f5d020';
-});
-b3.addEventListener('mouseover', function () {
-    bottom.style.background = '#e8522a';
-});
-
-b1.addEventListener('mouseout', function () { bottom.style.background = 'gray'; });
-b2.addEventListener('mouseout', function () { bottom.style.background = 'gray'; });
-b3.addEventListener('mouseout', function () { bottom.style.background = 'gray'; });
-
-
+let newWindow = null;
+ 
+document.getElementById('btnOpen').onclick = function() {
+    newWindow = window.open('https://itstep.org', '_blank', 'width=640,height=480');
+};
+ 
+document.getElementById('btnClose').onclick = function() {
+    if (newWindow != null) {
+        newWindow.close();
+        newWindow = null;
+    }
+};
+ 
 // Завдання 2
-var box = document.getElementById('box');
-
-document.addEventListener('mousemove', function (e) {
-    box.style.left = e.pageX - box.offsetWidth / 2 + 'px';
-    box.style.top  = e.pageY - box.offsetHeight / 2 + 'px';
-});
+let languages = navigator.languages;
+let mainLang = navigator.language;
+let list = document.getElementById('languageList');
+ 
+for (let i = 0; i < languages.length; i++) {
+    let item = document.createElement('li');
+    item.textContent = languages[i];
+ 
+    if (languages[i] === mainLang) {
+        item.className = 'mainLang';
+    }
+ 
+    list.appendChild(item);
+}
+ 
